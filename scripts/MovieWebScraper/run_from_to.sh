2c7288f11f52c7a163797c43ad0728f96f25d434
Riccardo Mancini <r.mancini@santannapisa.it>
2020-02-07 19:27:43 +0100

    Added simple bash script to execute range of movies with sleep

---
+++ scripts/MovieWebScraper/run_from_to.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -le 1 ]; then
+    echo "Usage ./run_from_to.sh <from> <to>"
+    echo "e.g. ./run_from_to.sh 0 12500"
+    echo "Sleep between requests is 5 seconds"
+    exit 1
+fi
+
+from=$1
+to=$2
+
+echo $from > UpdateIndex.txt
+
+while [ $(cat UpdateIndex.txt) -lt $to ]; do
+    sleep 5
+    echo "--- EXECUTING # $(cat UpdateIndex.txt)"
+    python MongoDbManager.py
+done
