cb910fbf435d3b19124dc23159e558e67cb956c0
Riccardo Mancini <r.mancini@santannapisa.it>
2020-03-18 21:40:09 +0100

    scraper: edit config file to point to new server

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,4 @@
 dataset_location = "datasets/"
 parse_dataset_output = "movies_imdb.json"
-mongo_uri = "mongodb://localhost:27017"
-mongo_db = "task2-test"
+mongo_uri = "mongodb://45.76.92.122:27017"
+mongo_db = "moviedb"
