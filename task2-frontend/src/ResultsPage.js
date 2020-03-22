e7a5443b028a2b132fe3257035eab6c5ededfe42
Mirko <mirkoletto@gmail.com>
2020-03-22 03:24:59 +0100

    Results with 'unsized' image

--- task2-frontend/src/ResultsPage.js
+++ task2-frontend/src/ResultsPage.js
@@ -2,14 +2,83 @@ import React, { Component } from 'react'
 
 import BasicPage from './BasicPage.js'
 import MyCard from './MyCard.js'
+import { List, ListItem, ListItemIcon, ListItemAvatar, Avatar, Divider, ListItemText, Typography } from '@material-ui/core'
+
+const styles = {
+    img: {
+        paddingRight: '10px',
+    }
+}
 
 class ResultsPage extends Component {
+    constructor() {
+        super();
+        this.queryOut = [
+            {
+                "_id": "tt7286456",
+                "title": "Joker",
+                "year": 2019,
+                "poster": "https://picsum.photos/130/180",
+                "genres": ["Crime", "Drama", "Thriller"],
+                "total_rating": 8.87,
+                "user_rating": 7
+            },
+            {
+                "_id": "tt7286456",
+                "title": "Joker",
+                "year": 2019,
+                "poster": "https://picsum.photos/130/179",
+                "genres": ["Crime", "Drama", "Thriller"],
+                "total_rating": 8.87,
+                "user_rating": 7
+            },
+            {
+                "_id": "tt7286456",
+                "title": "Joker",
+                "year": 2019,
+                "poster": "https://picsum.photos/130/181",
+                "genres": ["Crime", "Drama", "Thriller"],
+                "total_rating": 8.87,
+            },
+        ]
+    }
 
-    render(){
+    render() {
         return (
             <BasicPage>
                 <br />
-                <MyCard><br />Results for... I don't know</MyCard>
+                <MyCard>
+                    The best results are here for you:
+                    <List>
+                        {this.queryOut.map((data, index) => (
+                            <div key={index}>
+                                <ListItem alignItems="flex-start">
+                                    <ListItemAvatar children={
+                                        <img src={data.poster} style={styles.img} />
+                                    }>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        className={styles.itemText}
+                                        primary={data.title}
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="textPrimary"
+                                                >
+                                                    Ali Connors  {data.year}
+                                                </Typography>
+                                                {" — I'll be in your neighborhood doing errands this…"}
+                                            </React.Fragment>
+                                        }
+                                    />
+                                </ListItem>
+                                <Divider component="li" />
+                            </div >
+                        ))}
+                    </List>
+                </MyCard>
             </BasicPage>
         )
     }
