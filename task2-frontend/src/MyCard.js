e68bce3e25dfb19359b008458fa4e5689ac85300
Mirko <mirkoletto@gmail.com>
2020-03-24 19:10:01 +0100

    UX changes

--- task2-frontend/src/MyCard.js
+++ task2-frontend/src/MyCard.js
@@ -5,9 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
     paperRoot: {
         color: theme.palette.primary.main,
-    },
-    contentRoot: {
-        margin: '1em',
+        padding: '1em'
     }
 }))
 
