3f8dacddd92fa91b04d2aab6d64ff96b167b1c87
Riccardo Mancini <r.mancini@santannapisa.it>
2020-05-18 00:00:28 +0200

    Updated connection strings

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,4 @@
 dataset_location = "datasets/"
 parse_dataset_output = "movies_imdb.json"
-mongo_uri = "mongodb://lsmsdb:bdsmsl@45.76.92.122:27017"
+mongo_uri = "mongodb://lsmsdb:bdsmsl@172.16.1.162:27017,172.16.1.195:27017,172.16.1.251:27017/moviedb?replicaSet=replicaset0"
 mongo_db = "moviedb"
