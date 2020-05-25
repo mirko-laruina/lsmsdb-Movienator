41724512db0598faaaf1387df940ded41df95107
Riccardo Mancini <r.mancini@santannapisa.it>
2020-05-25 15:20:39 +0200

    neo4j_sync: explicit global declaration of graph

--- scripts/neo4j/synchronize.py
+++ scripts/neo4j/synchronize.py
@@ -11,20 +11,28 @@ import config
 # configuration
 keep_movie_attrs = ['_id', 'title', 'year', 'poster']
 keep_user_attrs  = ['_id', 'username']
-N_THREADS=4
+N_THREADS=10
+
+graph = None
 
 def sync_movie(m):
+    global graph
+
     movie = {k:v for k,v in m.items() if k in keep_movie_attrs}
     n = Node("Movie", **movie)
     graph.merge(n, "Movie", "_id")
 
 def sync_user(u):
+    global graph
+
     u['_id'] = u['_id'].__str__()
     user = {k:v for k,v in u.items() if k in keep_user_attrs}
     m = Node("User", **user)
     graph.merge(m, "User", "_id")
 
 def sync_rating(r):
+    global graph
+
     RATED = Relationship.type("RATED")
 
     u = graph.nodes.match("User", _id=r["_id"]["user_id"].__str__()).first()
