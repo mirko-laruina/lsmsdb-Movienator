41e36650aabcb356a4e013349bad117c165e71a4
Riccardo Mancini <r.mancini@santannapisa.it>
2020-05-25 14:49:37 +0200

    script_config: Added neo4j uri and auth

--- scripts/config.py
+++ scripts/config.py
@@ -2,3 +2,5 @@ dataset_location = "datasets/"
 parse_dataset_output = "movies_imdb.json"
 mongo_uri = "mongodb://lsmsdb:bdsmsl@172.16.1.162:27017,172.16.1.195:27017,172.16.1.251:27017/moviedb?replicaSet=replicaset0"
 mongo_db = "moviedb"
+neo4j_uri = "bolt://172.16.1.251:7687"
+neo4j_auth = ('neo4j', 'bdsmsl')
