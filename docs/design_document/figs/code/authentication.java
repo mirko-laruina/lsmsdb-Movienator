92d20fae36ba2388278743742f131513ab8e3a29
Riccardo Mancini <r.mancini@santannapisa.it>
2020-05-30 18:55:20 +0200

    doc: pushed missing update in authentication snippet

--- docs/design_document/figs/code/authentication.java
+++ docs/design_document/figs/code/authentication.java
@@ -15,27 +15,20 @@ usersCollection.updateOne(
     set("password", u.getPassword())
 );
 
-// 4 - authenticate user
-usersCollection.find(
-        and(eq("username", u.getUsername()),
-            eq("password", u.getPassword())
-    ))
-    .first();
-
-// 5 - add session
+// 4 - add session
 usersCollection.updateOne(
     eq("_id", u.getId()),
     push("sessions", Session.Adapter.toDBObject(s))
 );
 
-// 6 - finds the user whose session is s
+// 5 - finds the user whose session is s
 usersCollection.find(
         and(
             eq("sessions._id", s.getId())
         ))
     .first();
 
-// 7 - remove session
+// 6 - remove session
 usersCollection.updateOne(
     eq("_id", u.getId()),
     pull("sessions", eq("_id", s.getId()))
